8a3eb3aae6ff90157aa1e5aa9eb2131e0ca49b91
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-08-21 13:02:45 -0500

    added database.json

--- src/scripts/locations/deleteLocation.js
+++ src/scripts/locations/deleteLocation.js
@@ -2,7 +2,7 @@ let dataManager = require("../dataManager/dataManager")
 
 const deleteLocation = function () {
     document.querySelector("#locationsList").addEventListener("click", (event) => {
-        console.log("did it work")
+
         if(event.target.id.split("--")[0] === "delete"){
 
             let id = event.target.id.split("--")[1]
--- src/scripts/locations/locationCard.js
+++ src/scripts/locations/locationCard.js
@@ -1,6 +1,6 @@
 function locationCard(id, name, title, location, content) {
     return `
-        <div id="location--${id}" class="card text-center">
+        <div id="location--${id}" class="card">
             <div class="card-header">
                 <h3>${title}</h3>
                 <h4>${location}</h4>
@@ -10,11 +10,10 @@ function locationCard(id, name, title, location, content) {
             </div>
             <div class="card-footer">
                 <h5>Entered by: ${name}</h5>
-                <button id="edit--${id}">Edit</button>
                 <button id="delete--${id}">Delete</button>
             </div>
         </div>
-    `;
+    `
 }
 
 // This exports the body of the location card, so the next function knows how to build it.
